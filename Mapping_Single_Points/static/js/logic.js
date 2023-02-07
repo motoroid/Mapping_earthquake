1a745a19ac19ef7b5c49b2b9f5d2120139e8e522
motoroid <motoroid24@gmail.com>
2023-02-07 05:38:57 -0700

    module work

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -4,47 +4,65 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Alternate way to Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
 
-//add a circle marker instead
-L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: 'yellow'
- }).addTo(map);
+  // function drawMarker(city) {
+  //   L.marker(city.location)
+  //     .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+  //     .addTo(map);
+  // }
+  
+  // // Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+  // for (var i = 0; i < cities.length; i++) {
+  //   var city = cities[i];
+  //   drawMarker(city);
+  // }
+  
+// User forEach to Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
+cities.forEach(city =>   L.marker(city.location)
+.bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+.addTo(map)
+);
 
- //add a 10 pixel sized circle
-L.circleMarker([34.0522, -118.2437]).addTo(map);
 
 
-// We create the tile layer that will be the background of our map.
-//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
-    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    
-    //id: 'mapbox/streets-v11',
-    // //map style options 
-    // mapbox/outdoors-v11
-    // mapbox/light-v10
-    // mapbox/dark-v10
-    //id: 'mapbox/satellite-v9',
-    // mapbox/satellite-streets-v11
-    
-    //tileSize: 512,
-    //zoomOffset: -1,
-    
-    accessToken: API_KEY
-});
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
