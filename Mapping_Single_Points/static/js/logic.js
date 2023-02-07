66eb15dd281a406377207b55496016d23fbd2104
motoroid <motoroid24@gmail.com>
2023-02-06 18:53:09 -0700

    map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -12,8 +12,24 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add a circle marker instead
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: 'yellow'
+ }).addTo(map);
+
+ //add a 10 pixel sized circle
+L.circleMarker([34.0522, -118.2437]).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',{
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     
@@ -22,7 +38,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     // mapbox/outdoors-v11
     // mapbox/light-v10
     // mapbox/dark-v10
-    id: 'mapbox/satellite-v9',
+    //id: 'mapbox/satellite-v9',
     // mapbox/satellite-streets-v11
     
     //tileSize: 512,
