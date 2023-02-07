1963865fce69415e4b71136f2b779529368196d3
motoroid <motoroid24@gmail.com>
2023-02-07 09:12:09 -0700

    module work

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -13,39 +13,8 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(map);
 
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
-
+// Get data from cities.js
+let cityData = cities;
   // function drawMarker(city) {
   //   L.marker(city.location)
   //     .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
@@ -59,8 +28,8 @@ let cities = [{
   // }
   
 // User forEach to Loop through the cities array and create one marker for each city, bind a popup containing its name and population add it to the map
-cities.forEach(city =>   L.marker(city.location)
-.bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+cityData.forEach(city =>   L.circleMarker(city.location, {radius:city.population/100000})
+.bindPopup("<h1>" + city.city + ","+ city.state + "</h1> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
 .addTo(map)
 );
 
