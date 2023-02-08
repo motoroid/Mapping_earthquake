a88496cafb8a0c6f50baf81da948da9485c78fe0
motoroid <motoroid24@gmail.com>
2023-02-07 17:09:45 -0700

    toronto routes

---
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -0,0 +1,48 @@
+// Add console.log to check to see if our code is working.
+// console.log("working");
+
+// // Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+
+let streets=L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  maxZoom: 18,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+})
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+//****careful that this points to a branch on the repository not the main one in case it's a broken link
+
+let airportData = "https://raw.githubusercontent.com/motoroid/Mapping_earthquake/Mapping_Multiple_Points/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
