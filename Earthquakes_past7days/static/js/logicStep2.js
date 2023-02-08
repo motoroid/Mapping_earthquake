11e700b50f567fe415580353bedbbb6803fa4e6c
motoroid <motoroid24@gmail.com>
2023-02-08 10:31:36 -0700

    more earthquake format

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -50,5 +50,17 @@ d3.json(earthquake_7_days).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data).addTo(map);
-});
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+
+//   // We turn each feature into a circleMarker on the map.
+  
+//   pointToLayer: function(feature, latlng) {
+//               console.log(data);
+//               return L.circleMarker(latlng);
+//           },
+//       }).addTo(map);
+  });
+
+
 
